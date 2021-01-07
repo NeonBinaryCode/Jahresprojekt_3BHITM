cf31a11e3f65e1f1c6828ab918d2c6437661d840
it180184 <f.ettinger@students.htl-leonding.ac.at>
2021-01-07 02:05:55 +0100

    update link on date change

--- js/movie.js
+++ js/movie.js
@@ -1,25 +1,37 @@
-function fetchData() {
-    $.post('../api/get-movie.php', { id: getQuery('id') }, (data) => {
-        data = JSON.parse(data);
-        $('.movie-poster')[0].src = `../media/${data.poster}`;
-        $('.movie-title').text(data.title);
-        $('.movie-description').text(data.description);
-        $('#start-reservation')[0].href = `../book-seats/?id=${data.id}`;
-        let informationHtml = '';
-        for (const key in data.information) {
-            const val = data.information[key];
-            if (typeof val == 'object') {
-                for (let i = 0; i < val.length; i++) {
-                    informationHtml += '<tr><td>';
-                    if (i == 0) informationHtml += key;
-                    informationHtml += `</td><td>${val[i]}</td</tr>\n`;
+(() => {
+    let id = -1;
+    let selectDate = $('#select-date')[0];
+    function fetchData() {
+        $.post('../api/get-movie.php', { id: getQuery('id') }, (data) => {
+            data = JSON.parse(data);
+            $('.movie-poster')[0].src = `../media/${data.poster}`;
+            $('.movie-title').text(data.title);
+            $('.movie-description').text(data.description);
+            id = data.id;
+            updateLink();
+            let informationHtml = '';
+            for (const key in data.information) {
+                const val = data.information[key];
+                if (typeof val == 'object') {
+                    for (let i = 0; i < val.length; i++) {
+                        informationHtml += '<tr><td>';
+                        if (i == 0) informationHtml += key;
+                        informationHtml += `</td><td>${val[i]}</td</tr>\n`;
+                    }
+                } else {
+                    informationHtml += `<tr><td>${key}</td><td>${val}</td</tr>\n`;
                 }
-            } else {
-                informationHtml += `<tr><td>${key}</td><td>${val}</td</tr>\n`;
             }
-        }
-        $('.general-information').html(informationHtml);
-    });
-}
+            $('.general-information').html(informationHtml);
+        });
+    }
 
-fetchData();
+    fetchData();
+
+    selectDate.addEventListener('change', () => {
+    });
+
+    function updateLink() {
+        $('#start-reservation')[0].href = `../book-seats/?id=${id}&date=${encodeURIComponent(selectDate.value.replace(' ', '_'))}`;
+    }
+})();
